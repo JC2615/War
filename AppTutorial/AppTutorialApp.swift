e7353fd036bdd285b62ee0769d5be520ddb336b5
Joshua Curry <joshuacurry2615@gmail.com>
2021-08-03 16:25:43 -0400

    Initial Commit

---
+++ AppTutorial/AppTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTutorialApp.swift
+//  AppTutorial
+//
+//  Created by HBCU-c2-14 on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AppTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppTutorial
+//
+//  Created by HBCU-c2-14 on 8/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
