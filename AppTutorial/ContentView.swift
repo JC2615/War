9e15b3e8ba3c7687beca19a7aa49e8c149d70135
Joshua Curry <joshuacurry2615@gmail.com>
2021-08-03 17:20:52 -0400

    Made game UI

--- AppTutorial/ContentView.swift
+++ AppTutorial/ContentView.swift
@@ -9,9 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack() {
+            
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack(){
+                
+                Image("logo")
+                Spacer()
+                HStack(spacing: 50.0) {
+                    Image("card3")
+                    Image("card4")
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack(spacing: 50.0) {
+                    VStack(spacing: 25.0){
+                        Text("Player")
+                            .foregroundColor(Color.white)
+                            .fontWeight(.heavy)
+                        Text("0")
+                            .foregroundColor(Color.white)
+                            .fontWeight(.heavy)
+                    }
+                    VStack(spacing: 25.0){
+                        Text("CPU")
+                            .foregroundColor(Color.white)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
+                        Text("0")
+                            .foregroundColor(Color.white)
+                            .fontWeight(.heavy)
+                    }
+                }
+                
+                
+                Spacer()
+            }
+            
+            
+        }
+        
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
